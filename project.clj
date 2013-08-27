364bb31416b9f113edd03014c7cfca0339b9e8c8
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-08-27 12:55:04 -0400

    updated project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject btc-e-client "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject btc-e-client "0.1.0"
+  :description "Small btc-e client library"
+  :url "https://github.com/MarcoPolo/btc-e-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
