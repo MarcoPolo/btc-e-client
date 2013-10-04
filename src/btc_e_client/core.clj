ff4b02da3d3920ebb6484fdbd6a529ed7a55d636
Brian Picciano <bgp2396@bellsouth.net>
2013-10-04 12:07:05 -0400

    made api methods not use global variables, user specifies currency

--- src/btc_e_client/core.clj
+++ src/btc_e_client/core.clj
@@ -6,29 +6,30 @@
             [org.httpkit.client :as http]
             clojure.walk))
 
-(def secret atom "")
-(def api-key atom "")
+(defn init
+  "Initiates an api structure which can be used to interact with the api.
+  Currency is the currencies you want to interact in, for example :btc_usd,
+  :ltc-usd, etc... You can leave the secret and api-key as empty strings if you
+  just want to use the public api"
+  [currency secret api-key ]
+  { :curr currency :secret secret :key api-key})
 
 (def endpoint "https://btc-e.com/tapi")
 
-(def public-api {:ticker "https://btc-e.com/api/2/btc_usd/ticker"
-                 :trades "https://btc-e.com/api/2/btc_usd/trades"
-                 :fee "https://btc-e.com/api/2/btc_usd/fee"
-                 :depth "https://btc-e.com/api/2/btc_usd/depth"})
+(defn- public-api
+  [api endpt]
+  (str "https://btc-e.com/api/2/" (:curr api) "/" (name endpt)))
 
-
-(defn get-body-sync [url]
+(defn- get-body-sync [url]
   (json/read-str (:body @(http/get url)) :key-fn keyword))
 
-(defn get-ticker [] (get-body-sync (:ticker public-api)))
+(defn get-ticker [api] (get-body-sync (public-api api :ticker)))
 
-(defn get-trades [] (get-body-sync (:trades public-api)))
+(defn get-trades [api] (get-body-sync (public-api api :trades)))
 
-(defn get-fee [] (get-body-sync (:fee public-api)))
+(defn get-fee [api] (get-body-sync (public-api api :fee)))
 
-(defn get-depth [] (get-body-sync (:depth public-api)))
-
-
+(defn get-depth [api] (get-body-sync (public-api api :fee)))
 
 ;; This is how their api works
 
@@ -43,10 +44,10 @@
 ;; nonce : a nonce, using the unix ts in ms
 ;; method : method name
 ;; param : val
-;; param2 : val2 
+;; param2 : val2
 ;; ...
 
-(defn- postage 
+(defn- postage
   "Creates a post body"
   [param-map]
   (->> param-map
@@ -54,32 +55,33 @@
     (reduce #(str %1 "&" (first %2) "=" (second %2)) "")
     (#(subs % 1)))) ;; Get rid of the first "&" in "&param1=stuff"
 
-(defn- btc-e-request-raw [post-data]
+(defn- btc-e-request-raw [api post-data]
   ;; add the nonce to the data
   (let [post-data (str post-data "&nonce=" (int (/ (System/currentTimeMillis) 1000)))]
-    (http/post endpoint {:headers {"Key" api-key 
-                                   "Sign" (sha512-hmac post-data secret) 
+    (http/post endpoint {:headers {"Key" (:key api)
+                                   "Sign" (sha512-hmac post-data (:secret api))
                                    "Content-Type" "application/x-www-form-urlencoded"}
                          :body post-data})))
 
-(defn async-trade-api-request 
+(defn async-trade-api-request
   "Call the btc-e api"
-  ([method-name params]
-   (btc-e-request-raw (postage (assoc params "method" method-name))))
-  ([method-name]
-   (async-trade-api-request method-name {})))
+  ([api method-name params]
+   (btc-e-request-raw api (postage (assoc params "method" method-name))))
+  ([api method-name]
+   (async-trade-api-request api method-name {})))
 
 (defn trade-api-request
   "Synchronous version to call the api request"
-  ([method-name params]
-   (json/read-str (:body @(async-trade-api-request method-name params)) :key-fn keyword))
+  ([api method-name params]
+    (json/read-str
+      (:body @(async-trade-api-request api method-name params))
+      :key-fn keyword))
   ([method-name]
-   (trade-api-request method-name {})))
+   (trade-api-request api method-name {})))
 
 ;; Demo
 (comment
 
-  
   (require '[btc-e-client.core :as btce]) ;; load the library
 
   (btce/get-ticker) ;; Get the ticker
